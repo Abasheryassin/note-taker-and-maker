7a99c2c7944ce3d9fc0eb93d094ef3a24688a14e
abasheryassin <abasheryassin@gmail.com>
2023-01-17 18:31:18 -0600

    load notes page

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const db = require('./db/db.json');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true} ));
+
+app.use(express.static('public'));
+
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.listen(PORT, () => console.log(`Serving static asset routes at http://localhost:${PORT}`));
